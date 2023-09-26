8e76c59580359c936de3b92742844bff9a10acc8
liujf69 <50939145+liujf69@users.noreply.github.com>
2023-09-26 19:08:51 +0800

    Update postprocess.h

--- TRT_YoloV5/src/postprocess.h
+++ TRT_YoloV5/src/postprocess.h
@@ -9,7 +9,8 @@ void nms(std::vector<Detection>& res, float *output, float conf_thresh, float nm
 
 void batch_nms(std::vector<std::vector<Detection>>& batch_res, float *output, int batch_size, int output_size, float conf_thresh, float nms_thresh = 0.5);
 
-void draw_bbox(std::vector<cv::Mat>& img_batch, std::vector<std::vector<Detection>>& res_batch);
+void draw_batch_bbox(std::vector<cv::Mat>& img_batch, std::vector<std::vector<Detection>>& res_batch);
+void draw_bbox(cv::Mat& image, std::vector<Detection>& res);
 
 std::vector<cv::Mat> process_mask(const float* proto, int proto_size, std::vector<Detection>& dets);
 
